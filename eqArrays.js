eb5d36870870c054ef1de79b2783d0dc69a9e98b
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-10 21:19:53 -0500

    function for checking two arrays

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`😢 Assertion failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`😊 Assertion Passed: ${actual} === ${expected}`);
+  }
+  
+};
+
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true); 
