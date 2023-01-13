dd87e437c2d908c25a2376d74f08a46587aea939
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-13 02:27:33 -0500

    returns an array when the callback function is met

---
+++ takeUntil.js
@@ -0,0 +1,44 @@
+const takeUntil = function (array,conditions) {
+  let result = [];
+  for (let item of array) {
+    if(conditions(item)){
+      return result;
+    }
+    result.push(item);
+  }
+  return result;
+}
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1,array2) {
+  
+  if (eqArrays(array1,array2) === true) {
+    console.log(`🙌✔👏 Array Assertion passed, ${array1} and ${array2} are the same `);
+  } else {
+    console.log(`😢❌😭 Array Assertion fail, ${array1} and ${array2} are not the same`);
+  }
+
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ]);
+
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2,[ "I've", 'been', 'to', 'Hollywood']);
