fd605dea7bceb53f5544874028639d05d3895804
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-16 21:35:42 -0500

    centralized all the function into index.js

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countOnly = require('./countOnly'); 
+const findKeyByValue = require('./findKeyByValue');
+const map = require('./map');
+const without = require('./without');
+const eqArrays = require('./eqArrays');
+const assertObjectsEqual  = require('./assertObjectsEqual');
+const eqObjects  = require('./eqObjects');
+const takeUntil = require('./takeUntil');
+const countLetters = require('./countLetters');
+const findKey = require('./findKey');
+const letterPositions = require('./letterPositions');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,
+  tail,
+  middle,
+  assertArraysEqual,  
+  countOnly,
+  findKeyByValue,
+  map, 
+  without,
+  assertEqual,
+  eqArrays,
+  head ,
+  middle, 
+  tail,
+  assertObjectsEqual,  
+  eqObjects  ,
+  takeUntil,
+  countLetters,  
+  findKey ,
+  letterPositions, 
 };
+
+console.log(module);
